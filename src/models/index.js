1ccc75442a80430a2b953dec5bae1eeec0ae9737
Ahmed Ehab <92032849+ahmedehhab@users.noreply.github.com>
2026-02-22 17:01:49 +0200

    Feature/review model (#10)
    * review model
    
    * add timestamps
    
    ---------
    
    Co-authored-by: Karim-308 <105867500+Karim-308@users.noreply.github.com>

--- src/models/index.js
+++ src/models/index.js
@@ -1,4 +1,5 @@
 module.exports.User = require('./user.model');
 module.exports.Token = require('./token.model');
 module.exports.Book = require('./book.model');
+module.exports.Review = require('./review.model');
 module.exports.Author = require('./author.model');
