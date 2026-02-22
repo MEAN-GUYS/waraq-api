179c058f171caa884bf2d7a408d11b8647d97765
ahmed ehab <ahmedehab4778@gmail.com>
2026-02-22 15:15:50 +0200

    review model

---
+++ src/models/review.model.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const reviewSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  book: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Book',
+    required: true,
+  },
+  rating: {
+    type: Number,
+    required: true,
+    min: 1,
+    max: 5,
+  },
+  comment: {
+    type: String,
+    maxlength: 500,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Review = mongoose.model('Review', reviewSchema);
+module.exports = Review;
